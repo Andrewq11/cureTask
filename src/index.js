7fcb91ecd8174caad5f22cdcbf8a8a337e524308
Andrewq11 <andrewquirke99@gmail.com>
2021-10-01 00:24:22 -0400

    Wrapping app tree in hashrouting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 // Initial entry point of app
+// Wrapping in HashRouter for github pages functionality 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
